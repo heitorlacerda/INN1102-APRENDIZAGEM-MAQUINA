3f194dec5cdc81359b47d7a3b70e57e9b2456f8c
heitorlacerda <heitorlacerda@gmail.com>
2016-11-20 11:04:33 -0300

    Atualização baseado na Mariana

--- abalone_dados.py
+++ abalone_dados.py
@@ -1,43 +1,16 @@
-from numpy import double
-import numpy as np
+def read_data (database_file):
+	database = open (database_file)
+	X = []
+	y = []
+	line = database.readline ()
 
-class DataMgr:
-    """
-    Class responsible for importing right data from text bases.
-    """
-    
-    def __init__(self):
-        with open("../datasets/mfeat-fac.txt") as fac_f:
-            self.dataSet_fac = []
-            for line in fac_f:
-                self.dataSet_fac.append(list(map(int, line.split()))) 
-                           
-        print("Imported fac")   
-        
-        with open("../datasets/mfeat-fou.txt") as fou_f:
-            self.dataSet_fou = []
-            for line in fou_f:
-                self.dataSet_fou.append(list(map(double, line.split()))) 
-                               
-            print("Imported fou")  
-            
-        with open("../datasets/mfeat-kar.txt") as kar_f:
-            self.dataSet_kar = []
-            for line in kar_f:
-                self.dataSet_kar.append(list(map(double, line.split()))) 
-                               
-            print("Imported kar")   
-            
-        with open("../datasets/labels.txt") as labels_f:
-            self.dataSet_labels = []
-            for line in labels_f:
-                self.dataSet_labels.append(int(line.split()[0]))
-                               
-            print("Imported labels")  
-    
-    def rawData(self):
-        """
-        
-        """
-            
-        return np.array(self.dataSet_labels), [np.array(self.dataSet_fac), np.array(self.dataSet_fou), np.array(self.dataSet_kar)]
+	while line:
+		l = line.strip().split(',')
+		x = l[1:]
+		x = map (float, x)
+		X.append (x)
+		y.append (l[0].strip())
+		line = database.readline()
+	
+	database.close ()
+	return X,y
--- cs.py
+++ cs.py
@@ -1,134 +1,41 @@
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.cross_validation import StratifiedKFold
+import sys
+import os.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+from sklearn.neural_network import MLPClassifier
+from sklearn import svm
 from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import LinearSVC
-from sklearn.preprocessing import LabelEncoder
-from sknn.mlp import Classifier as MLPClassifier
-from sknn.mlp import Layer
-from datasets.read_from_file import DataMgr
-from scipy.stats import friedmanchisquare
-from scipy.stats.stats import rankdata
-from nemenyi import critical_difference
-import numpy as np
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.ensemble import VotingClassifier
+from scipy import stats
+from numpy import sqrt
 
-# Parameters
-alpha = 0.01    # Nemenye alpha
-n_folds = 40    # Number of folds for Cross Validation
+from read_data import *
+from normalize import *
 
-# Classifiers
-layers=[Layer("Maxout", units=100, pieces=2),Layer("Sigmoid")]
-clf1 = MLPClassifier(layers=layers,learning_rate=0.001,n_iter=25)
-clf2 = OneVsRestClassifier(LinearSVC(random_state=0))
-clf3 = OneVsRestClassifier(GaussianNB())
-clfs = [clf1, clf2, clf3]
+X,y = read_data ("../abalone.data")
 
-# Import Data
-dataMgr = DataMgr()
-y, X = dataMgr.rawData()
+X = normalize (X,7)
+X = normalize (X,3)
 
-# Create folds for Stratified cross validation
-skf = StratifiedKFold(y, n_folds=n_folds, shuffle=False, random_state=None)
+net = 	MLPClassifier(verbose=False, activation='logistic', validation_fraction=0.33, hidden_layer_sizes=(5, ), early_stopping=True, learning_rate='constant', learning_rate_init=0.2, max_iter=500, momentum=0.9)
 
-# Encode label on binary form for multilabel classification
-le = LabelEncoder()
-le.fit(y)
+SVM = svm.SVC ()
+GNB = GaussianNB()
 
-# Create list of lists of scores for each classfier
-scores = [[] for _ in clfs]
+cv = StratifiedKFold(n_splits=10)
 
-# Create list for score of the combination of individual classifiers
-globalScores = []
+scores_net = cross_val_score(net, X, y, cv=cv, scoring = 'accuracy')
+scores_SVM = cross_val_score(SVM, X, y, cv=cv, scoring = 'accuracy')
+scores_GNB = cross_val_score(GNB, X, y, cv=cv, scoring = 'accuracy')
 
-# Fold counter just for printing
-fold = 1
+ic_net = stats.norm.interval(0.95, loc=1-scores_net.mean(), scale=scores_net.std()/sqrt(len(X)))
+ic_SVM = stats.norm.interval(0.95, loc=1-scores_SVM.mean(), scale=scores_SVM.std()/sqrt(len(X)))
+ic_GNB = stats.norm.interval(0.95, loc=1-scores_GNB.mean(), scale=scores_GNB.std()/sqrt(len(X)))
 
-# For each fold run the classification on all data views using each classfier
-# Then combine the results on each data view using majority voting
-# Finally, after all classifiers have ran, combine their results again using majority voting
-for train_index, test_index in skf:
-    globalPred = []
-    print("Fold = " + str(fold))
-    fold = fold + 1
-    # Obtain Labels of training and testing
-    y_train, y_test = y[train_index], y[test_index]
-    
-    # Run each classifier
-    for clf, score in zip(clfs, scores):
-        predictions = []
-        
-        # For each data view
-        for p in range(len(X)):
-            
-            # Obtain Features for training and for testing
-            X_train, X_test = X[p][train_index], X[p][test_index]
-            
-            # Train Classifier
-            clf.fit(X_train, y_train)
-            
-            # Add classifier prediction on list
-            predictions.append(clf.predict(X_test))
-        
-        # Combine prediction on all data views using majority voting
-        predictions = np.asarray(predictions).T
-        maj = np.apply_along_axis(lambda x:
-                                  np.argmax(np.bincount(x)),
-                                  axis=1,
-                                  arr=predictions)
-        maj = le.inverse_transform(maj)
-        
-        # Add final classifier prediction on global list
-        globalPred.append(maj)
-        
-        # Add classification score on list
-        score.append(accuracy_score(y_test, maj))
-    
-    # Combine prediction on all classifiers using majority voting
-    globalPred = np.asarray(globalPred).T
-    globalMaj = np.apply_along_axis(lambda x:
-                                  np.argmax(np.bincount(x)),
-                                  axis=1,
-                                  arr=globalPred)
-    globalMaj = le.inverse_transform(globalMaj)
-    
-    # Add combined classification score on list
-    globalScores.append(accuracy_score(y_test, globalMaj))
+print("Error MLP: %0.2f (+/- %0.2f)" % (1-scores_net.mean(), scores_net.mean() - ic_net[0]))
+print("Error SVM: %0.2f (+/- %0.2f)" % (1-scores_SVM.mean(), scores_SVM.mean() - ic_SVM[0]))
+print("Error GNB: %0.2f (+/- %0.2f)" % (1-scores_GNB.mean(), scores_GNB.mean() - ic_GNB[0]))
 
-# After all classifications. Add global scores on score list as a new classifier
-scores.append(globalScores)
-
-# Convert scores to np.array for Friedman Test Calculation
-scores = np.array(scores)
-
-# Calculate errors from test scores.
-errors = np.array([np.subtract(1, score) for score in scores])
-
-# Print accuracy mean and deviation based on all scores evaluation
-for score, label in zip(scores, ['MLP', 'Linear SVM', 'naive Bayes', 'Ensemble']):
-    print("Accuracy: %0.2f (+/- %0.2f) [%s]" % (np.mean(score), np.std(score), label))
-  
-# Print Scores of all classifiers on each fold
-for scoreLn in scores.T:
-    print(scoreLn)
-    
-argument = [scores[i, :] for i in np.arange((scores).shape[0])]
-
-# Rank data
-data1 = np.vstack(argument).T
-data1 = data1.astype(float)
-for i in range(len(data1)):
-    data1[i] = rankdata(data1[i])
-print(data1)
-
-# Calculate Friedman Test mean Rank and pValue
-chi, pValue = friedmanchisquare(*argument)
-
-print("Friedman Test: Chi = " + str(chi) + ", pValue = " + str(pValue))
-
-# If pre test is valid, use Nemenyi port-test for final decition
-if(pValue <= alpha):
-    print("Para alpha = " + str(alpha) + " Hipotese nula nao foi confirmada")
-    cd = critical_difference(pvalue=alpha, models=len(scores), datasets=n_folds)
-    print("Nemenyi: CD = " + str(cd))
-    if(chi > cd):
-        print("Pelo Nemenyi Test, hipotese nula foi descartada.")
+print stats.friedmanchisquare(scores_net, scores_SVM, scores_GNB)
